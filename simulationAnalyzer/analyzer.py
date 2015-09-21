348cc58ab03f3f1c47b1aedd4891d77cad5b6227
smcho <sm.cho@mac.com>
2015-09-21 07:13:38 -0500

    Utility initial version OK

--- simulationAnalyzer/analyzer.py
+++ simulationAnalyzer/analyzer.py
@@ -8,10 +8,25 @@ class Analyzer(object):
         self.reader = converter.reader
         self.jsonMap = self.reader.jsonMap
         self.groupCount = self.reader.groupCount
-        pass
 
     def toContextNames(self, contexts):
+        """
+        The contexts from JSON summary (using getContexts) is a list of a lot of contexts,
+        they should be organized and be shown in a digestive way.
 
+        Given contexts from JSON summary, this function returns the names of contexts
+        in a readable way.
+
+        This example shows the conversion from context name (g10c -> v1), and the context count.
+
+        [('v1', 4)]
+        [('p1', 3), ('p2', 3), ('p3', 3), ...
+        ...
+        [('md1', 3)]
+
+        :param contexts:
+        :return:
+        """
         def grp(txt):
             r = re.search("\w(\d+)", txt)
             return int(r.group(1))
@@ -32,10 +47,21 @@ class Analyzer(object):
         return res
 
     def getContexts(self, groupId, index):
+        """
+
+        From JSON result, we need to extract the contexts from groupId/index pair.
+        For example, the first host in the "V" group (v/1) will be translated into context name
+        such as g1c0 (first group, first host), as the JSON hosts stores information using the
+        context name format.
+
+        :param groupId:
+        :param index:
+        :return:
+        """
         host = self.converter.groupIDIndexToHost(groupId, index)
         return self.jsonMap["hostToTuplesMap"][str(host)]
 
-    def getTime(self, groupId1, index1, groupId2, index2):
+    def showTime(self, groupId1, index1, groupId2, index2):
         """
             returns the time when
             groupId1/index1 finds the contexts of groupId2/index2
