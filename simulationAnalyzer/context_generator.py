b2d7fff7201cd38203c6d6b53b6fb6f66c96b38b
smcho <sm.cho@mac.com>
2015-09-28 13:25:02 -0500

    Start ConfigGenerator

--- simulationAnalyzer/context_generator.py
+++ simulationAnalyzer/context_generator.py
@@ -1,11 +1,11 @@
 __author__ = 'smcho'
 
-from simulationAnalyzer.context import *
 from simulationAnalyzer.generator import *
+import glob
 
 class ContextGenerator(Generator):
 
-    def __init__(self, simulationName, strategy, id, controlName):
+    def __init__(self, simulationName, strategy, id):
         super(self.__class__, self).__init__(simulationName, strategy, id)
         self.jsonFilePaths = glob.glob(self.path.getContextPoolDirectory() + "*.json")
         self.path.getContextDirectory()
